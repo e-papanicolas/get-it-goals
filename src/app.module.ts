0b936ac847b6245cc55a77bfc77c0b963a0decf7
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-10-18 15:31:51 -0400

    working through documentation, adding starter and example code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).exclude('auth').forRoutes(UsersController);
+    // OR:
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .forRoutes({ path: 'users', method: RequestMethod.GET });
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './interfaces/user.interface';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [];
+
+  create(createUserDto: CreateUserDto) {
+    this.users.push();
+    return 'This action adds a new user';
+  }
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
